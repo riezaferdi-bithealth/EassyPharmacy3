15e3f47904200c86c3a00e1952a73f880bcb6f80
riezaferdi-bithealth <rieza.ferdiansyah@bithealth.co.id>
2024-03-26 13:50:29 +0700

    testing

---
+++ Flutter/flutter_eassypharmacy/lib/core/core.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_eassypharmacy/core/core.dart';
--- Flutter/flutter_eassypharmacy/lib/main.dart
+++ Flutter/flutter_eassypharmacy/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_eassypharmacy/core/core.dart';
 
 void main() {
   runApp(const MainApp());
