6d58f73f746d3c6669be3e95213fd3da89d8b0d9
ilyasaarinto <ilyasa.arinto@bithealth.co.id>
2024-05-13 20:17:09 +0700

    fixing response for get all obat dan search

--- backend0/controllers/obatController.js
+++ backend0/controllers/obatController.js
@@ -30,7 +30,21 @@ const getAllObat = async (req, res) => {
             options.offset = offset
         }
         const obats = await Obat.findAll(options);
-        res.status(200).json(obats);
+        const formattedObats = obats.map(obat => ({
+            id: obat.id,
+            name: obat.name,
+            description: obat.description,
+            price: obat.price,
+            stock: parseInt(obat.stock), // Mengonversi string ke integer
+            image: obat.image
+        }));
+
+        const response = {
+            status: true,
+            message: "success",
+            data: formattedObats
+        };
+        res.status(200).json(response);
     } catch (error) {
         console.error("Error in getAllObat:", error);
         res.status(500).json({ message: 'Internal Server Error' });
